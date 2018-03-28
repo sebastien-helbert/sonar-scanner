d3b418301a80b294a6d6dcf219bac056b883ae09
sebastien.helbert <sebastien.helbert@netapsys.fr>
2018-03-28 14:00:12 +0200

    1st commit

---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+set -eu 
+IFS=$'\n\t'
+
+if [ "$1" = 'sonar-scanner' ]; then
+	if [ -z "${SONAR_HOST_URL-}" ]; then
+	  echo ">>> Environment variable SONAR_HOST_URL is mandatory."
+	  exit 1
+	else
+	  export SONAR_SCANNER_OPTS="${SONAR_SCANNER_OPTS} -Dsonar.host.url=$SONAR_HOST_URL"
+	fi
+	
+	if [ -z "${SONAR_PROJECT_KEY-}" ]; then
+	  echo ">>> Environment variable SONAR_PROJECT_KEY is mandatory."
+	  exit 1
+	else
+	  export SONAR_SCANNER_OPTS="${SONAR_SCANNER_OPTS} -Dsonar.projectKey=$SONAR_PROJECT_KEY"
+	fi
+	
+	if [ -z "${SONAR_SOURCES-}" ]; then
+	  echo ">>> Environment variable SONAR_SOURCES, using /sonar-sources as default source directory."
+	  export SONAR_SCANNER_OPTS="${SONAR_SCANNER_OPTS} -Dsonar.sources=/sonar-sources"
+	else
+	  export SONAR_SCANNER_OPTS="${SONAR_SCANNER_OPTS} -Dsonar.sources=$SONAR_SOURCES"
+	fi
+	
+	if [ ! -z "${SONAR_LOGIN-}" ]; then
+	  export SONAR_SCANNER_OPTS="${SONAR_SCANNER_OPTS} -Dsonar.login=$SONAR_LOGIN"
+	fi
+	
+	if [ ! -z "${SONAR_PASSWORD-}" ]; then
+	  export SONAR_SCANNER_OPTS="${SONAR_SCANNER_OPTS} -Dsonar.password=$SONAR_PASSWORD"
+	fi
+fi
+
+exec "$@"
